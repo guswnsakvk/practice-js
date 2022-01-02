432e79311f4bd6ecc1bd8fa447172febd6646c80
guswnsakvk <guswnsakvk1@naver.com>
2022-01-02 21:01:52 +0900

    Bug fix

--- tab/main.js
+++ tab/main.js
@@ -2,21 +2,13 @@ const list = document.querySelector(".list")
 const tab = document.querySelectorAll(".tab-button")
 const text = document.querySelectorAll(".tab-content")
 
-tab[0].addEventListener("click", (event) => {
-  tab[0].classList.toggle("active")
-  text[0].classList.toggle("show")
-})
-
-tab[1].addEventListener("click", (event) => {
-  tab[1].classList.toggle("active")
-  text[1].classList.toggle("show")
-})
-
-tab[2].addEventListener("click", (event) => {
-  tab[2].classList.toggle("active")
-  text[2].classList.toggle("show")
-})
-
-// list.addEventListener("click", (event) => {
-//   event.target.classList.toggle("active")
-// })
+for (let i=0; i<tab.length;i++){
+  tab[i].addEventListener("click", (event) => {
+    for(let j=0;j<tab.length;j++){
+      tab[j].classList.remove("active")
+      text[j].classList.remove("show")
+    }
+    tab[i].classList.toggle("active")
+    text[i].classList.toggle("show")
+  })
+}
