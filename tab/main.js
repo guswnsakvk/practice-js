61cf94230f966aba66684ddbc34c300b49632440
guswnsakvk <guswnsakvk1@naver.com>
2022-01-03 17:25:03 +0900

    event bubbling

--- tab/main.js
+++ tab/main.js
@@ -2,13 +2,31 @@ const list = document.querySelector(".list")
 const tab = document.querySelectorAll(".tab-button")
 const text = document.querySelectorAll(".tab-content")
 
-for (let i=0; i<tab.length;i++){
-  tab[i].addEventListener("click", (event) => {
-    for(let j=0;j<tab.length;j++){
-      tab[j].classList.remove("active")
-      text[j].classList.remove("show")
-    }
-    tab[i].classList.toggle("active")
-    text[i].classList.toggle("show")
-  })
+// for문 tab기능
+// for (let i=0; i<tab.length;i++){
+//   tab[i].addEventListener("click", (event) => {
+//     for(let j=0;j<tab.length;j++){
+//       tab[j].classList.remove("active")
+//       text[j].classList.remove("show")
+//     }
+//     tab[i].classList.toggle("active")
+//     text[i].classList.toggle("show")
+//   })
+// }
+
+function checkTab(value){
+  for (let i=0;i<tab.length;i++){  
+    tab[i].classList.remove("active")
+    text[i].classList.remove("show")
+  }
+  tab[value].classList.add("active")
+  text[value].classList.add("show")
 }
+
+list.addEventListener("click", (event) => {
+  for(let i=0; i<tab.length;i++){
+    if (event.target === tab[i]){
+      checkTab(i)
+    }
+  }
+})
