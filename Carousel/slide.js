761fe022c059132745d35c3408ff34fa99931aac
guswnsakvk <guswnsakvk1@naver.com>
2022-01-11 15:59:32 +0900

    slide touch effect

--- Carousel/slide.js
+++ Carousel/slide.js
@@ -1,11 +1,28 @@
-var image1 = document.querySelectorAll('.slide-box img')[0];
+const image1 = document.querySelectorAll('.slide-box img')[0];
+const container = document.querySelector(".slide-container")
 
-var mc = new Hammer.Manager(image1);
+const mc = new Hammer.Manager(image1);
 mc.add(new Hammer.Pan({ threshold: 0 }));
     
-mc.on('pan', ()=>{
-  console.log("이미자 pan되었어요")
+function slide(value){
+  container.style.transform = `translateX(${value}px)`
+}
+
+function slide2(){
+  container.style.transform = "translateX(-100vw)"
+}
+
+mc.on('pan', (e)=>{
+  const move = e.deltaX
+  if(move < -1){
+    slide(e.deltaX)
+    if(e.isFinal){
+      container.classList.add("transforming")
+      slide2()
+      setTimeout(() => {
+        container.classList.remove("transforming")
+      },500)
+    }
+  }
 })
-// mc.on('pan', function(e){
-//   console.log("이미자가 pan되었어요") 
-// })
+
