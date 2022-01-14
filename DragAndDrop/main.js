e4f2ba09a9716bb8d3f7a6d793000b89e7c1b793
guswnsakvk <guswnsakvk1@naver.com>
2022-01-14 16:33:48 +0900

    DragAndDrop

---
+++ DragAndDrop/main.js
@@ -0,0 +1,17 @@
+function ondragstart_handler(event){
+  event.dataTransfer.setData("test", event.target.id);
+  console.log("dragStart")
+}
+
+function ondragover_handler(event){
+  event.preventDefault()
+  event.dataTransfer.dropEffect = "move"
+}
+
+function ondrop_handler(event){
+  const data = event.dataTransfer.getData("test")
+  const test1 = document.querySelector(`#${data}`)
+  const test2 = document.querySelector(`#${data} img`)
+  console.log(test1.innerText)
+  console.log(test2.src)
+}
