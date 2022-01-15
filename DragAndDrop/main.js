d9943a99026789a7e77b948f6adeeea0c204b556
guswnsakvk <guswnsakvk1@naver.com>
2022-01-15 20:04:53 +0900

    test: find parentNode

--- DragAndDrop/main.js
+++ DragAndDrop/main.js
@@ -12,6 +12,7 @@ function ondrop_handler(event){
   const data = event.dataTransfer.getData("test")
   const test1 = document.querySelector(`#${data}`)
   const test2 = document.querySelector(`#${data} img`)
+  console.log(test1.parentNode.id)
   console.log(test1.innerText)
   console.log(test2.src)
 }
