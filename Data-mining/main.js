c9b8ef7865ac282a2752dd0d15ae125420e2109a
guswnsakvk <guswnsakvk1@naver.com>
2022-01-26 11:23:22 +0900

    using foreach

--- Data-mining/main.js
+++ Data-mining/main.js
@@ -2,18 +2,25 @@ const input = document.querySelector("input")
 const getValue = document.querySelector(".getValue")
 const dataObj = {}
 const h1 = document.querySelector("h1")
-let count = 1
+// let count = 1
 
 getValue.addEventListener("submit", function(event){
   event.preventDefault()
   const lst = [...input.value].sort()
-  for(let i=1;i<=lst.length;i++){
-    if(lst[i-1] != lst[i]){
-      dataObj[lst[i-1]] = count
-      count = 1
+  // for(let i=1;i<=lst.length;i++){
+  //   if(lst[i-1] != lst[i]){
+  //     dataObj[lst[i-1]] = count
+  //     count = 1
+  //   } else{
+  //     count += 1
+  //   }
+  // }
+  lst.forEach(function(element){
+    if(dataObj[element] > 0){
+      dataObj[element]++
     } else{
-      count += 1
+      dataObj[element] = 1
     }
-  }
+  })
   console.log(dataObj)
 })
