60d46bd19a958014c0c27c1534885b90dd86262b
guswnsakvk <guswnsakvk1@naver.com>
2022-03-13 23:15:14 +0900

    data binding

--- MovieApi/main.js
+++ MovieApi/main.js
@@ -11,8 +11,11 @@ function fetchMovies(){
     .get('https://www.omdbapi.com/?apikey=7035c60c&s=frozen')
     .then((response) => {
       console.log(response)
+      const movie = document.querySelector("h1")
+      const moviePost = document.querySelector("img")
+      movie.innerText = response.data.Search[0].Title
+      moviePost.src = response.data.Search[0].Poster
     })
-    .catch(error => console.error(error))
 }
 
 fetchMovies()
