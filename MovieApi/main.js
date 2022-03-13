d5ad9c9144b3c6bc7e1b4c40bb41a793fcd977d4
guswnsakvk <guswnsakvk1@naver.com>
2022-03-13 23:32:00 +0900

    getMovie test

--- MovieApi/main.js
+++ MovieApi/main.js
@@ -1,14 +1,17 @@
 const getMovie = document.querySelector(".getMovie")
-let moveName = document.querySelector(".movieName")
+let getMovieName = document.querySelector(".getMovieName")
+let movieName = ''
 
 getMovie.addEventListener("submit", function(event){
   event.preventDefault()
-  console.log(moveName.value)
+  movieName = getMovieName.value
+  console.log(movieName)
+  fetchMovies(movieName)
 })
 
-function fetchMovies(){
+function fetchMovies(movieName){
   axios
-    .get('https://www.omdbapi.com/?apikey=7035c60c&s=frozen')
+    .get(`https://www.omdbapi.com/?apikey=7035c60c&s=${movieName}`)
     .then((response) => {
       console.log(response)
       const movie = document.querySelector("h1")
@@ -18,4 +21,3 @@ function fetchMovies(){
     })
 }
 
-fetchMovies()
