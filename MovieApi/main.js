fa6568bd64e86baf8a1fddab4b93a83e3dbf8da9
guswnsakvk <guswnsakvk1@naver.com>
2022-03-14 10:38:36 +0900

    sorting moviePoster

--- MovieApi/main.js
+++ MovieApi/main.js
@@ -1,12 +1,14 @@
 const getMovie = document.querySelector(".getMovie")
 let getMovieName = document.querySelector(".getMovieName")
 let movieName = ''
+const movieBox = document.querySelector(".movieBox")
 
 getMovie.addEventListener("submit", function(event){
   event.preventDefault()
   movieName = getMovieName.value
   console.log(movieName)
   fetchMovies(movieName)
+  getMovieName.value = ''
 })
 
 function fetchMovies(movieName){
@@ -14,10 +16,25 @@ function fetchMovies(movieName){
     .get(`https://www.omdbapi.com/?apikey=7035c60c&s=${movieName}`)
     .then((response) => {
       console.log(response)
-      const movie = document.querySelector("h1")
-      const moviePost = document.querySelector("img")
-      movie.innerText = response.data.Search[0].Title
-      moviePost.src = response.data.Search[0].Poster
+      while( movieBox.hasChildNodes()){
+        movieBox.removeChild(movieBox.firstChild)
+      }
+      const moviePosters = []
+      for(let i=0; i<response.data.Search.length;i++){
+        moviePosters.push(response.data.Search[i].Poster)
+      }
+      console.log(moviePosters)
+      console.log(response.data)
+
+      moviePosters.forEach(function(item) {
+        const moviePost = document.createElement("div")
+        moviePost.classList.add("movePost")
+        const moviePoster = document.createElement("img")
+        moviePoster.src = item
+        console.log(item)
+        moviePost.append(moviePoster)
+        movieBox.append(moviePost)
+      })
     })
 }
 
