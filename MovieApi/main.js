854ec317ec201929ba783df2ec54f0b9fc501415
guswnsakvk <guswnsakvk1@naver.com>
2022-03-14 15:19:05 +0900

    Movie details

--- MovieApi/main.js
+++ MovieApi/main.js
@@ -1,7 +1,11 @@
 const getMovie = document.querySelector(".getMovie")
 let getMovieName = document.querySelector(".getMovieName")
 let movieName = ''
+const OMDBKEY = '7035c60c'
 const movieBox = document.querySelector(".movieBox")
+const selectedMoviePoster = document.querySelector(".selectedMoviePoster")
+const movieTitle = document.querySelector(".movieTitle")
+const moviePlot = document.querySelector(".moviePlot")
 
 getMovie.addEventListener("submit", function(event){
   event.preventDefault()
@@ -13,7 +17,7 @@ getMovie.addEventListener("submit", function(event){
 
 function fetchMovies(movieName){
   axios
-    .get(`https://www.omdbapi.com/?apikey=7035c60c&s=${movieName}`)
+    .get(`https://www.omdbapi.com/?apikey=${OMDBKEY}&s=${movieName}`)
     .then((response) => {
       console.log(response)
       while( movieBox.hasChildNodes()){
@@ -27,15 +31,28 @@ function fetchMovies(movieName){
       console.log(moviePosters)
       console.log(response.data)
 
-      moviePosters.forEach(function(item) {
+      moviePosters.forEach(function(movie) {
         const moviePost = document.createElement("div")
         moviePost.classList.add("movePost")
         const moviePoster = document.createElement("img")
-        moviePoster.src = item.Poster
-        console.log(item)
+        moviePoster.src = movie.Poster
+        moviePoster.addEventListener("click", function(){
+          viewInfo(movie)
+        })
+        console.log(movie)
         moviePost.append(moviePoster)
         movieBox.append(moviePost)
       })
     })
 }
 
+function viewInfo(movie){
+  selectedMoviePoster.src = movie.Poster
+  movieTitle.innerText = movie.Title
+  axios
+    .get(`https://www.omdbapi.com/?apikey=${OMDBKEY}&i=${movie.imdbID}`)
+    .then((response) => {
+      console.log(response.data)
+      moviePlot.innerText = response.data.Plot
+    })
+}
